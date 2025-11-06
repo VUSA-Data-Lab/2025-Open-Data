99820f7cc349193be67ef7a1463239e55437a113
MykolasOK <173080194+MykolasOK@users.noreply.github.com>
2025-11-06 15:09:07 +0200

    Update select-env.sh to install Python and comment out lines
    Python + PHP installation update

--- .devcontainer/select-env.sh
+++ .devcontainer/select-env.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo
+# echo
+# apt-get update -y
+
 echo "Activating Python environment..."
+apt-get install -y python3 python3-pip
 pip install pandas matplotlib requests jupyterlab yfinance
 echo "✅ Python ready!"
 echo
+
 echo "Activating PHP environment..."
-sudo apt-get update -y
 sudo apt-get install -y php-cli composer
 echo "✅ PHP ready!"
 echo
